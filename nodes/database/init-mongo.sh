37f0efba34e2aa8fa64faecf7eebbea6d32efae6
Mirko Mollik <mollik@trustcerts.de>
2022-07-18 11:09:27 +0200

    fix: use sh instead of js file for mongo
    Signed-off-by: Mirko Mollik <mollik@trustcerts.de>

---
+++ nodes/database/init-mongo.sh
@@ -0,0 +1,12 @@
+set -e
+
+mongo <<EOF
+db.createUser({
+  user: '$MONGO_INITDB_ROOT_USERNAME',
+  pwd: '$MONGO_INITDB_ROOT_PASSWORD',
+  roles: [{
+    role: 'readWrite',
+    db: '$MONGO_INITDB_DATABASE'
+  }]
+})
+EOF
